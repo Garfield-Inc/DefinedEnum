dd5f9f35da5ea10b22013f3e0dc5a6a7b9240c05
Soumya Satish <88482100+soumya-embold@users.noreply.github.com>
2021-11-22 16:01:26 +0530

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class abc
+{
+	int m_currentLogMask[10];
+	enum xyz
+	{
+		DEFINED_ENUM = 0,
+		DEFINED_ENUM_2 = 0x80000000,
+		DEFINED_ENUM_3 = 3
+	};
+
+	void fun();
+};
+
---
+++ Source.cpp
@@ -0,0 +1,7 @@
+#include "Header.h"
+
+void abc::fun()
+{
+	int m_currentLogMask[3];
+	m_currentLogMask[DEFINED_ENUM_2] |= DEFINED_ENUM;
+}
